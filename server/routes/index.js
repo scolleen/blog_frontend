80b88889be412341c959452d38a9024d7f481101
xiupeiyin <yinxiupei@yunwii.com>
2018-07-18 11:00:40 +0800

    Modify: 重新设网站布局，将koa部分与vue前端分离出来，删除多余的字体文和图片

--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-var Router = require('koa-router')
-var data = require('./index.json')
-const router = new Router()
-
-router.get('/info', (ctx, next) => {
-  ctx.body = data
-})
-module.exports = router
